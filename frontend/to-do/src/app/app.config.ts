c599b5652c8450c500c2ff8baafa47bdab9ecbd1
Sydney Cendana <103895652+sydneycendana@users.noreply.github.com>
2024-01-22 14:15:11 -0800

    fixed angular server errors

--- frontend/to-do/src/app/app.config.ts
+++ frontend/to-do/src/app/app.config.ts
@@ -3,12 +3,17 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { HttpClientModule, provideHttpClient } from '@angular/common/http';
+import {
+  HttpClientModule,
+  provideHttpClient,
+  withFetch,
+} from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideClientHydration(),
     importProvidersFrom(HttpClientModule),
+    provideHttpClient(withFetch()),
   ],
 };
--- frontend/to-do/src/app/auth.service.ts
+++ frontend/to-do/src/app/auth.service.ts
@@ -1,16 +1,23 @@
-import { Injectable } from '@angular/core';
+import { Injectable, PLATFORM_ID, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { isPlatformBrowser } from '@angular/common';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   private apiUrl = 'http://localhost:3000/api/user';
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {}
 
   get isLoggedIn(): boolean {
-    return !!localStorage.getItem('token');
+    if (isPlatformBrowser(this.platformId)) {
+      return !!localStorage.getItem('token');
+    }
+    return false; // Not on the browser, return false by default
   }
 
   login(credentials: { username: string; password: string }): Observable<any> {
